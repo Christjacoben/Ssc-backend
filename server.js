231093f93ab1350d9b528a5b980452cef6cd112a
Cris <111942487+Christjacoben@users.noreply.github.com>
2025-01-13 16:59:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const MONGO_URI =
   "mongodb+srv://admin:admin123@ssc.hirsz.mongodb.net/?retryWrites=true&w=majority&appName=SSC";
 
 const corsOptions = {
-  origin: "www.ssceventsattendance.com",
+  origin: "https://www.ssceventsattendance.com",
   credentials: true,
 };
 
